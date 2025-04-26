ef7ba3fa0cda7c5e25007e7e849973408ec48234
YexScript <yexelespinosa12@gmail.com>
2025-04-26 18:36:23 +0800

    Update YexScript.lua

--- YexScript.lua
+++ YexScript.lua
@@ -1,9 +1,9 @@
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 
 local Window = Rayfield:CreateWindow({
-    Name = "YexScript Hub",
-    LoadingTitle = "YexScript Hub Loading...",
-    LoadingSubtitle = "Join Discord: discord.gg/gAJSCeZT",
+    Name = "YexScript Hub | Blox Fruits",
+    LoadingTitle = "YexScript Hub",
+    LoadingSubtitle = "by Yex",
     ConfigurationSaving = {
         Enabled = true,
         FolderName = "YexScriptHub",
@@ -11,14 +11,14 @@ local Window = Rayfield:CreateWindow({
     },
     Discord = {
         Enabled = true,
-        Invite = "gAJSCeZT",
+        Invite = "gAJSCezt", -- Correct invite
         RememberJoins = true
     },
     KeySystem = true,
     KeySettings = {
         Title = "YexScript Key System",
-        Subtitle = "Join Discord: discord.gg/gAJSCeZT",
-        Note = "Key = YexScript145",
+        Subtitle = "Join Discord: discord.gg/gAJSCezt",
+        Note = "Join our Discord server to get the key!",
         FileName = "YexScriptKey",
         SaveKey = true,
         GrabKeyFromSite = false,
@@ -26,231 +26,173 @@ local Window = Rayfield:CreateWindow({
     }
 })
 
-local MainTab = Window:CreateTab("Main")
-local TeleportTab = Window:CreateTab("Teleport")
-local DevilFruitTab = Window:CreateTab("Devil Fruit")
-local MiscTab = Window:CreateTab("Misc")
-local StatusTab = Window:CreateTab("Status")
-local SettingsTab = Window:CreateTab("Settings")
+local attackSpeed = 0.16 -- Default Attack Speed
 
--- Variables
-local AttackSpeed = 0.15
-local SelectedWeapon = "Melee"
-local AutoFarm = false
-local AutoAttack = false
+local MainTab = Window:CreateTab("Main", 4483362458)
 
--- Auto Farm Function
-function StartAutoFarm()
-    spawn(function()
-        while AutoFarm do
-            pcall(function()
-                local playerLevel = game.Players.LocalPlayer.Data.Level.Value
-                local QuestName, MobName, MobCFrame = nil, nil, nil
-
-                for i,v in pairs(Quests) do
-                    if playerLevel >= v.Level[1] and playerLevel <= v.Level[2] then
-                        QuestName = v.QuestName
-                        MobName = v.MobName
-                        MobCFrame = v.CFrame
-                        break
-                    end
-                end
-
-                if QuestName and MobName then
-                    if not game.Players.LocalPlayer.PlayerGui.Main.Quest.Visible then
-                        local args = {
-                            [1] = QuestName
-                        }
-                        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("StartQuest", unpack(args))
-                    end
-
-                    for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
-                        if v.Name == MobName and v:FindFirstChild("Humanoid") and v.Humanoid.Health > 0 then
-                            repeat task.wait(AttackSpeed)
-                                pcall(function()
-                                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame * CFrame.new(0,5,0)
-                                    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("Attack",SelectedWeapon)
-                                end)
-                            until v.Humanoid.Health <= 0 or not AutoFarm
-                        end
-                    end
-                end
-            end)
-            task.wait()
-        end
-    end)
-end
-
--- Attack Speed
-MainTab:CreateDropdown({
-    Name = "Attack Speed",
-    Options = {"Normal (6/sec)", "Fast (8/sec)", "Super Fast (10/sec)"},
-    CurrentOption = "Normal (6/sec)",
-    Callback = function(option)
-        if option == "Normal (6/sec)" then
-            AttackSpeed = 0.15
-        elseif option == "Fast (8/sec)" then
-            AttackSpeed = 0.12
-        elseif option == "Super Fast (10/sec)" then
-            AttackSpeed = 0.1
-        end
-    end
-})
-
--- Weapon Selector
-MainTab:CreateDropdown({
-    Name = "Select Weapon",
-    Options = {"Melee","Sword","Gun","Fruit"},
-    CurrentOption = "Melee",
-    Callback = function(option)
-        SelectedWeapon = option
-    end
-})
-
--- Auto Farm Toggle
-MainTab:CreateToggle({
+local ToggleFarm = MainTab:CreateToggle({
     Name = "Auto Farm Level",
     CurrentValue = false,
     Callback = function(Value)
-        AutoFarm = Value
-        if AutoFarm then
-            StartAutoFarm()
-        end
-    end
-})
-
--- Teleport Tab
-for Sea,Islands in pairs(IslandList) do
-    TeleportTab:CreateSection(Sea)
-    for i,v in pairs(Islands) do
-        TeleportTab:CreateButton({
-            Name = v.Name,
-            Callback = function()
-                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
-            end
-        })
-    end
-end
-
--- Devil Fruit Tab
-DevilFruitTab:CreateButton({
-    Name = "Teleport To Spawned Fruit",
-    Callback = function()
-        for i,v in pairs(game.Workspace:GetChildren()) do
-            if string.find(v.Name, "Fruit") then
-                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
-            end
-        end
-    end
-})
-
-DevilFruitTab:CreateToggle({
-    Name = "Auto Store Fruits",
-    CurrentValue = false,
-    Callback = function(Value)
-        if Value then
-            spawn(function()
-                while task.wait(1) do
-                    for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
-                        if string.find(v.Name, "Fruit") then
-                            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("StoreFruit", v.Name)
-                        end
-                    end
-                end
+        _G.AutoFarm = Value
+        while _G.AutoFarm do
+            pcall(function()
+                local level = game.Players.LocalPlayer.Data.Level.Value
+                -- Auto Quest + Kill NPC logic here
             end)
+            wait(1)
         end
+    end
+})
+
+local AttackSpeedDropdown = MainTab:CreateDropdown({
+    Name = "Attack Speed",
+    Options = {"Normal (6/sec)", "Fast (8/sec)", "Super Fast (10/sec)"},
+    CurrentOption = "Normal (6/sec)",
+    Callback = function(Option)
+        if Option == "Normal (6/sec)" then
+            attackSpeed = 0.16
+        elseif Option == "Fast (8/sec)" then
+            attackSpeed = 0.12
+        elseif Option == "Super Fast (10/sec)" then
+            attackSpeed = 0.1
+        end
+    end
+})
+
+local WeaponSelector = MainTab:CreateDropdown({
+    Name = "Select Weapon",
+    Options = {"Melee", "Sword", "Gun", "Fruit"},
+    CurrentOption = "Melee",
+    Callback = function(Value)
+        _G.SelectedWeapon = Value
+    end
+})
+
+local DevilFruitTab = Window:CreateTab("Devil Fruit", 4483362458)
+
+DevilFruitTab:CreateButton({
+    Name = "Auto Teleport To Fruit (Spawned)",
+    Callback = function()
+        -- Teleport Tween to Fruit Logic
+    end
+})
+
+DevilFruitTab:CreateButton({
+    Name = "Auto Grab Dropped Fruit (Bypass)",
+    Callback = function()
+        -- Auto grab dropped fruits bypass logic
+    end
+})
+
+DevilFruitTab:CreateButton({
+    Name = "Auto Store Fruit",
+    Callback = function()
+        -- Auto Store Fruit
     end
 })
 
 DevilFruitTab:CreateButton({
     Name = "Auto Random Fruit",
     Callback = function()
-        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("Cousin", "Buy")
+        -- Auto Random Fruit
     end
 })
 
--- Misc Tab
+local TeleportTab = Window:CreateTab("Teleport", 4483362458)
+
+TeleportTab:CreateButton({
+    Name = "Teleport to Starter Island",
+    Callback = function()
+        -- Teleport Logic
+    end
+})
+
+TeleportTab:CreateButton({
+    Name = "Teleport to Jungle",
+    Callback = function()
+        -- Teleport Logic
+    end
+})
+
+local MiscTab = Window:CreateTab("Misc", 4483362458)
+
 MiscTab:CreateSlider({
     Name = "Farm Distance",
-    Range = {0,35},
+    Range = {5, 35},
     Increment = 1,
-    CurrentValue = 10,
+    CurrentValue = 15,
     Callback = function(Value)
-        FarmDistance = Value
+        _G.FarmDistance = Value
     end
 })
 
 MiscTab:CreateSlider({
     Name = "Tween Speed",
-    Range = {1,300},
+    Range = {1, 300},
     Increment = 1,
-    CurrentValue = 100,
+    CurrentValue = 150,
     Callback = function(Value)
-        TweenSpeed = Value
+        _G.TweenSpeed = Value
     end
 })
 
 MiscTab:CreateSlider({
     Name = "Bring Mob Distance",
-    Range = {1,300},
-    Increment = 1,
+    Range = {10, 300},
+    Increment = 5,
     CurrentValue = 100,
     Callback = function(Value)
-        BringDistance = Value
+        _G.BringMobDistance = Value
     end
 })
 
--- Status Tab
-spawn(function()
-    while task.wait(2) do
-        local Mirage = game:GetService("ReplicatedStorage"):FindFirstChild("Mirage Island") ~= nil
-        local Kitsune = game:GetService("ReplicatedStorage"):FindFirstChild("Kitsune Island") ~= nil
-        local FullMoon = game.Lighting:FindFirstChild("FullMoon") ~= nil
-        local Dealer = game:GetService("Workspace").NPCs:FindFirstChild("Legendary Sword Dealer") ~= nil
+local StatusTab = Window:CreateTab("Status", 4483362458)
 
-        StatusTab:CreateLabel("Mirage Island: " .. tostring(Mirage))
-        StatusTab:CreateLabel("Kitsune Island: " .. tostring(Kitsune))
-        StatusTab:CreateLabel("Full Moon: " .. tostring(FullMoon))
-        StatusTab:CreateLabel("Sword Dealer (Second Sea): " .. tostring(Dealer))
+StatusTab:CreateParagraph({
+    Title = "Real-Time Status",
+    Content = "Mirage: Checking...\nKitsune Island: Checking...\nFull Moon: Checking...\nSword Dealer: Checking..."
+})
+
+spawn(function()
+    while true do
+        -- Real-time check logic
+        wait(5)
     end
 end)
 
--- Settings Tab
+local SettingsTab = Window:CreateTab("Settings", 4483362458)
+
 SettingsTab:CreateButton({
-    Name = "Rejoin",
+    Name = "Rejoin Server",
     Callback = function()
-        game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+        game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
     end
 })
 
 SettingsTab:CreateButton({
     Name = "Server Hop",
     Callback = function()
-        local HttpService = game:GetService("HttpService")
-        local TeleportService = game:GetService("TeleportService")
-        local PlaceID = game.PlaceId
-        local Servers = HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/"..PlaceID.."/servers/Public?sortOrder=Asc&limit=100"))
-        for i,v in pairs(Servers.data) do
-            if v.playing < v.maxPlayers then
-                TeleportService:TeleportToPlaceInstance(PlaceID, v.id, game.Players.LocalPlayer)
-                break
-            end
-        end
-    end
-})
-
-SettingsTab:CreateButton({
-    Name = "Join Discord Server",
-    Callback = function()
-        Rayfield:Notify({Title = "Discord", Content = "Join: discord.gg/gAJSCeZT"})
+        -- Server Hop logic
     end
 })
 
 SettingsTab:CreateInput({
     Name = "Join Job ID",
-    PlaceholderText = "Paste JobId Here",
-    RemoveTextAfterFocusLost = true,
-    Callback = function(Input)
-        game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, Input, game.Players.LocalPlayer)
+    PlaceholderText = "Enter Job ID",
+    RemoveTextAfterFocusLost = false,
+    Callback = function(Value)
+        game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, Value)
+    end
+})
+
+SettingsTab:CreateButton({
+    Name = "Join Discord",
+    Callback = function()
+        Rayfield:Notify({
+            Title = "Discord Join",
+            Content = "Join Discord: discord.gg/gAJSCezt",
+            Duration = 5
+        })
     end
 })
