150c13092f8b2637ecc00e0b159e96340e957d16
YexScript <yexelespinosa12@gmail.com>
2025-04-30 22:03:52 +0800

    Update YexScript.lua

--- YexScript.lua
+++ YexScript.lua
@@ -1,36 +1,20 @@
-if game.PlaceId ~= 2753915549 and game.PlaceId ~= 4442272183 and game.PlaceId ~= 7449423635 then
-    return warn("This script only runs in Blox Fruits.")
-end
-
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
-repeat task.wait(2) until Rayfield
+repeat task.wait() until Rayfield
 
 local Window = Rayfield:CreateWindow({
-   Name = "YexScript HUB",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "YexScript is loading...",
-   LoadingSubtitle = "By Yexhub",
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+   Name = "YexScript Hub | Blox Fruits",
+   LoadingTitle = "Loading...",
+   LoadingSubtitle = "Please wait",
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
+      FolderName = "YexScript",
+      FileName = "YexHub"
    },
    Discord = {
-      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "https://discord.gg/hsRQNYtx", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+      Enabled = false
    },
-   KeySystem = true, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "YexScript| Key System",
-      Subtitle = "Key System",
-      Note = "Join discord to key get key https://discord.gg/hsRQNYtx", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"https://pastebin.com/raw/eQZjuxk1"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+   KeySystem = false
 })
+
+local Tab = Window:CreateTab("Main", 4483362458)
+Tab:CreateLabel("YexScript is loaded successfully!")
