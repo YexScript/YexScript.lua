64a25411de062e2e77c56d124376609775f93d0b
YexScript <yexelespinosa12@gmail.com>
2025-04-30 20:11:01 +0800

    Update YexScript.lua

--- YexScript.lua
+++ YexScript.lua
@@ -29,6 +29,3 @@ local Window = Rayfield:CreateWindow({
       Key = {"https://pastebin.com/raw/eQZjuxk1"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
-
-
-   
